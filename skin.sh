26cca5a7c139cfcedf32d096cc1599af99c3c02a
Matt Hyclak <matt.hyclak@cbts.net>
2014-08-13 17:02:39 -0400

    Fix additional source problems

--- skin.sh
+++ skin.sh
@@ -5,14 +5,14 @@ pushd /var/www/miq/vmdb/public
 
 # Deploy Images
 IMPATH="images/layout/"
-FILES="RH-Product-Name.png VDCInternalBanner.jpg brand.svg login-screenground.jpg login-screen-logo.png"
+FILES="RH-Product-Name.png VDCInternalBanner.jpg brand.svg login-screen-background.jpg login-screen-logo.png"
 
 for f in $FILES; do 
   cp $IMPATH/$f $IMPATH/${f}.back
   install $sourcedir/$f $IMPATH/$f 
 done
 
-patch -p0 < login.patch
-patch -p0 < template.patch
+patch -p0 < $sourcedir/login.patch
+patch -p0 < $sourcedir/template.patch
 
 popd
