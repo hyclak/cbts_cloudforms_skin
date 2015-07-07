c51a2e6738d23de29d16c5d0b8b7b309effa663a
Matt Hyclak <matt.hyclak@cbts.net>
2015-07-07 11:59:32 -0400

    Update for CloudForms 3.2 (cfme 5.4)
    Header background image is not addressed until further research on patternfly can be done.

--- applybranding.sh
+++ applybranding.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #
-# Applies CBTS branding to Cloud Forms 3.1 (cfme 5.3)
+# Applies CBTS branding to CloudForms 3.2 (cfme 5.4)
 #
 # Version History
 #    0.1 - mrh - Initial Release
 #    0.2 - mrh - evm:compile_assets task doesn't like .orig files to be created, moving to separate backup dir
+#    0.3 - mrh - Update to CloudForms 3.2
 
-rpm -q patch >/dev/null
-if [ $? -ne 0 ]; then
-  echo "patch command is not installed. Installing patch rpm."
-  yum -y install patch
-fi
+#rpm -q patch >/dev/null
+#if [ $? -ne 0 ]; then
+#  echo "patch command is not installed. Installing patch rpm."
+#  yum -y install patch
+#fi
 
 if [ ! -d backup ]; then
   mkdir backup
@@ -23,29 +24,16 @@ pushd /var/www/miq/vmdb
 echo "Deploying Images"
 
 # Backup the default branding if it hasn't been done
-if [ ! -f productization/assets/images/layout/brand.svg ]; then
-  mv productization/assets/images/layout/brand.svg ${sourcedir}/backup/brand.svg.orig
+if [ ! -f ${sourcedir}/backup/brand.svg.orig ]; then
+  mv productization/assets/images/brand.svg ${sourcedir}/backup/brand.svg.orig
 fi
 
 # Deploy CBTS Brand in upper left corner
-install -m 644 $sourcedir/brand.svg productization/assets/images/layout/brand.svg
+install -m 644 $sourcedir/brand.svg productization/assets/images/brand.svg
 
 # Deploy CBTS Header Banner
-install -m 644 $sourcedir/VDCInternalBanner.jpg public/images/layout/VDCInternalBanner.jpg
-
-# Patch the CSS
-echo "Replacing CSS files"
-for file in header.css.erb login.css.erb; do
-  if [ ! -f productization/assets/stylesheets/${file}.orig ]; then
-    mv productization/assets/stylesheets/${file} ${sourcedir}/backup/${file}.orig
-  fi
-
-  install -m 644 $sourcedir/${file} productization/assets/stylesheets/${file}
-done
-
-# Patch the UI Constants (to make the top border Orange by default)
-echo "Patching UI Constants"
-patch -p0 < $sourcedir/constants.diff
+# TODO: This was handled in header.css.erb but is now done in patternfly. Need to figure out how to override that.
+# install -m 644 $sourcedir/VDCInternalBanner.jpg public/images/layout/VDCInternalBanner.jpg
 
 # Rebuild assets in Cloud Forms
 echo "Rebuilding UI Assets"
